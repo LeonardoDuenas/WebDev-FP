81332c9f568c4b7869e8fac6449c8dd17f596915
Abdelraouf Abdelgader <100388535+zikail@users.noreply.github.com>
2023-04-10 22:14:03 -0400

    Update script.js

--- ChromaPersonality/ChromaPersonality/script.js
+++ ChromaPersonality/ChromaPersonality/script.js
@@ -9,28 +9,7 @@ function validate() {
     }
   }
        
-        
-
-
-        // Function to validate login credentials
-        function login() {
-            username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-            if (users[username] === password) {
-                document.getElementById("loginForm").style.display = "none";
-                document.getElementById("test").style.display = "block";
-                return false;
-            } else {
-                alert("Invalid login credentials. Please try again.");
-                return false;
-            }
-        }
-
-        // Function to submit answers and calculate score
-        
-           
-       
-        
+             
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
